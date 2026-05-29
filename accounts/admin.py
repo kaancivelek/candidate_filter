cf78d42c35d960ce3525f2a379fdd103a06c5b08
kaancivelek <businesskaancivelek@gmail.com>
2026-05-29 16:03:29 +0300

    Enhance UserAdmin and forms for better user management in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'created_at')
-    list_filter = ('is_staff', 'is_active')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('-created_at',)
+class UserAdmin(DjangoUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active", "created_at")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined", "created_at")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,30 @@
 from django import core
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("email", "first_name", "last_name")
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions",
+        )
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
