94bff9b9f84c1c6fdbba5be1441f0264736a4bbf
louai aldalati <louai.workmail@gmail.com>
2026-05-29 16:39:58 +0300

    Fix user admin password hashing

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password # استيراد دالة التشفير
 from .models import User
 
 @admin.register(User)
@@ -7,3 +8,12 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ('is_staff', 'is_active')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('-created_at',)
+
+    # هذه الدالة تتدخل قبل الحفظ لتشفير كلمة المرور
+    def save_model(self, request, obj, form, change):
+        # التحقق مما إذا كانت كلمة المرور موجودة ولم يتم تشفيرها مسبقاً
+        if obj.password and not obj.password.startswith('pbkdf2_'):
+            obj.password = make_password(obj.password)
+        
+        # إكمال عملية الحفظ الطبيعية
+        super().save_model(request, obj, form, change)
